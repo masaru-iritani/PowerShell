4dcf98da242efdab10e1a23b5475de8aa6564e4a
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2020-04-24 22:22:02 +0900

    Check the module readiness

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,22 @@
 Set-StrictMode -Version "Latest"
 
-Import-Module -Name "posh-git"
-Import-Module -Name "PSReadLine"
+function global:reload() {
+    . $PSCommandPath
+}
+
+# Load modules.
+@(
+    "posh-git",
+    "PSReadLine"
+) | ForEach-Object -Process {
+    if (Get-Module -Name $_) {
+        Import-Module -Name $_
+    } else {
+        Write-Warning -Message "$_ module is not installed.  Skipped."
+    }
+}
+
+# Configure modules.
 Set-PSReadlineOption -EditMode Emacs
 
 Out-Host -InputObject "Loaded $PSCommandPath."
